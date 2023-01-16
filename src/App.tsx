963672f74a9f01d1f91e2cc048f02e2884211e1a
denysfmalta <denysfmalta@hotmail.com>
2023-01-16 17:36:43 -0300

    basic styling of the pages #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from './assets/image 2.png';
+import logo from './assets/compass-logo.png';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+       
+      <div className="text-container">
+          <h1>Welcome,</h1>
+          <p>Please, register to continue</p>
+      </div>
+       <div className="image-container">
+        <img src={image} className="image" alt="neon-notebook" />
+       </div>
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
