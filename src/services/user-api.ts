01fae82bd3dcaeab45464625a3e0117b68e57fde
denysfmalta <denysfmalta@hotmail.com>
2023-02-17 11:41:06 -0300

    request from API

--- src/services/user-api.ts
+++ src/services/user-api.ts
@@ -2,5 +2,16 @@ import axios from "axios";
 import { createData } from "./api-types";
 
 export const UserApi = {
+    SingUp: async (data: createData) => {
+        const response = await axios.post(
+          `https://latam-challenge-2.deta.dev/api/v1/users/sign-up`,
+          data
+        );
+    
+        return response;
+  }
+
+  SingIn: () => {
 
   },
+};
