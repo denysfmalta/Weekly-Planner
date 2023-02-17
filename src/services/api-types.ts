34962c3dea8b233a354102328050faf139271056
denysfmalta <denysfmalta@hotmail.com>
2023-02-17 11:41:38 -0300

    typing the register's API

---
+++ src/services/api-types.ts
@@ -0,0 +1,11 @@
+export type createData = {
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+    city: string;
+    country: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  };
+  
